67d345636cc2637d1c6aa82887568cae642640cd
RohanPatel-1 <88309713+RohanPatel-1@users.noreply.github.com>
2022-05-02 16:29:57 +1200

    Created repo for Python Purifier extension

---
+++ server/server.py
@@ -0,0 +1,388 @@
+############################################################################
+# Copyright(c) Open Law Library. All rights reserved.                      #
+# See ThirdPartyNotices.txt in the project root for additional notices.    #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License")           #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http: // www.apache.org/licenses/LICENSE-2.0                         #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+import asyncio
+from doctest import script_from_examples
+import json
+import re
+import time
+import uuid
+from json import JSONDecodeError
+from typing import Optional,List
+
+from pathlib import Path
+from pygls import*
+#from WDL import load
+from qchecker.match import aggregate_match_types
+from qchecker.substructures import *
+
+
+
+
+from pygls.lsp.methods import (COMPLETION, TEXT_DOCUMENT_DID_CHANGE,
+                               TEXT_DOCUMENT_DID_CLOSE, TEXT_DOCUMENT_DID_OPEN, 
+                               TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,DOCUMENT_HIGHLIGHT,HOVER)
+from pygls.lsp.types import (CompletionItem, CompletionList, CompletionOptions,
+                             CompletionParams, ConfigurationItem,
+                             ConfigurationParams, Diagnostic,
+                             DidChangeTextDocumentParams,
+                             DidCloseTextDocumentParams,
+                             DidOpenTextDocumentParams, MessageType, Position,
+                             Range, Registration, RegistrationParams,
+                             SemanticTokens, SemanticTokensLegend, SemanticTokensParams,
+                             Unregistration, TextDocumentItem,UnregistrationParams,Hover,TextDocumentPositionParams,DocumentHighlight,DocumentHighlightParams,Position,Range,DocumentHighlightOptions,DocumentHighlightKind)
+                             
+from pygls.lsp.types.basic_structures import (WorkDoneProgressBegin,
+                                              WorkDoneProgressEnd,  
+                                              WorkDoneProgressReport)
+from pygls.server import LanguageServer
+
+#from utils import code_checks,get_error_cordinates
+#from .server import code_checker
+
+COUNT_DOWN_START_IN_SECONDS = 10
+COUNT_DOWN_SLEEP_IN_SECONDS = 1
+
+
+class PythonLanguageServer(LanguageServer):
+    CMD_COUNT_DOWN_BLOCKING = 'countDownBlocking'
+    CMD_COUNT_DOWN_NON_BLOCKING = 'countDownNonBlocking'
+    CMD_PROGRESS = 'progress'
+    CMD_REGISTER_COMPLETIONS = 'registerCompletions'
+    CMD_SHOW_CONFIGURATION_ASYNC = 'showConfigurationAsync'
+    CMD_SHOW_CONFIGURATION_CALLBACK = 'showConfigurationCallback'
+    CMD_SHOW_CONFIGURATION_THREAD = 'showConfigurationThread'
+    CMD_UNREGISTER_COMPLETIONS = 'unregisterCompletions'
+
+    CONFIGURATION_SECTION = 'pythonServer'
+    HOVER = 'textDocument/hover'
+    DOCUMENT_HIGHLIGHT = 'textDocument/documentHighlight'
+
+    def __init__(self):
+        super().__init__()
+
+def doc_to_string(ls,params):
+    #text_doc = ls.workspace.get_document(params.text_document.uri)
+    #uri1 = params.text_document.uri
+    #doc = ls.workspace.get_document(uri1)
+    #doc1 = params.TextDocumentPositionParams
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+    string = open(text_doc.path, 'r').read()
+    #return  doc1
+    #return load(uri1.uri, source_text=doc.text)
+
+    #return text_object.text
+    return string
+
+
+def code_checks(code):
+    matches = []
+    for substructure in SUBSTRUCTURES:
+        matches += substructure.iter_matches(code)
+    return matches
+
+def get_error_cordinates(matches):
+    x = []
+    for match in matches:
+        #print(type(match))
+        temp = match.text_range
+        x.append([temp.from_line,temp.from_offset,temp.to_line,temp.to_offset])
+
+    return x
+
+python_server = PythonLanguageServer()
+@python_server.feature(HOVER)
+def hover(ls, params: TextDocumentPositionParams
+) -> Optional[Hover]:
+    code = doc_to_string(ls,params).strip()
+    #range = pygls_utils.current_word_range(document, params.position)
+    
+    matches = code_checks(code)
+    ranges = get_error_cordinates(matches)
+    lsp_ranges = [Range(start=Position(line=x[0]-1, character=x[1]),end=Position(line=x[2]-1, character=x[3])) for x in ranges]
+
+    for i in range(len(ranges)):
+        if params.position.line ==  ranges[i][0] or params.position.line ==  ranges[i][2]:
+            #Hover(range = lsp_ranges[i])
+            ls.show_message_log('HOVER')
+
+    return 
+            
+# text_object:  TextDocumentItem
+@python_server.feature(DOCUMENT_HIGHLIGHT)
+def highlight(ls, params: DocumentHighlightParams
+) -> Optional[List[DocumentHighlight]]:
+
+    # Test to see when function is triggered.     
+    ls.show_message_log('woooooooooooooo!')
+    # This code will be generated using doc_to_string() in utils once the function is completed
+    #code2 = TextDocumentItem(uri = params.text_document.uri)
+    code = doc_to_string(ls,params).strip()
+    
+    matches = code_checks(code)
+    ranges = get_error_cordinates(matches)
+    lsp_ranges = [Range(start=Position(line=x[0]-1, character=x[1]),end=Position(line=x[2]-1, character=x[3])) for x in ranges]
+
+    highlight_names = [
+        DocumentHighlight(range=lsp_range)
+        for lsp_range in lsp_ranges
+        if lsp_range
+    ]
+    return highlight_names if highlight_names else None 
+
+
+
+
+
+def _validate(ls, params):
+    ls.show_message_log('Validating json...')
+
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+
+    source = text_doc.source
+    diagnostics = _validate_json(source) if source else []
+    #highlight(ls, params=DocumentHighlightParams)
+
+    ls.publish_diagnostics(text_doc.uri, diagnostics)
+
+
+def _validate_json(source):
+    """Validates json file."""
+    diagnostics = []
+
+    try:
+        json.loads(source)
+    except JSONDecodeError as err:
+        msg = err.msg
+        col = err.colno
+        line = err.lineno
+
+        d = Diagnostic(
+            range=Range(
+                start=Position(line=line - 1, character=col - 1),
+                end=Position(line=line - 1, character=col)
+            ),
+            message=msg,
+            source=type(python_server).__name__
+        )
+
+        diagnostics.append(d)
+
+    return diagnostics
+
+
+@python_server.feature(COMPLETION, CompletionOptions(trigger_characters=[',']))
+def completions(params: Optional[CompletionParams] = None) -> CompletionList:
+    """Returns completion items."""
+    return CompletionList(
+        is_incomplete=False,
+        items=[
+            CompletionItem(label='"'),
+            CompletionItem(label='['),
+            CompletionItem(label=']'),
+            CompletionItem(label='{'),
+            CompletionItem(label='}'),
+        ]
+    )
+
+
+@python_server.command(PythonLanguageServer.CMD_COUNT_DOWN_BLOCKING)
+def count_down_10_seconds_blocking(ls, *args):
+    """Starts counting down and showing message synchronously.
+    It will `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        time.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@python_server.command(PythonLanguageServer.CMD_COUNT_DOWN_NON_BLOCKING)
+async def count_down_10_seconds_non_blocking(ls, *args):
+    """Starts counting down and showing message asynchronously.
+    It won't `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        await asyncio.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@python_server.feature(TEXT_DOCUMENT_DID_CHANGE)
+def did_change(ls, params: DidChangeTextDocumentParams):
+    """Text document did change notification."""
+    _validate(ls, params)
+
+
+@python_server.feature(TEXT_DOCUMENT_DID_CLOSE)
+def did_close(server: PythonLanguageServer, params: DidCloseTextDocumentParams):
+    """Text document did close notification."""
+    server.show_message('Text Document Did Close')
+
+
+@python_server.feature(TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls, params: DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    ls.show_message('Text Document Did Open')
+    _validate(ls, params)
+
+
+@python_server.feature(
+    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
+    SemanticTokensLegend(
+        token_types = ["operator"],
+        token_modifiers = []
+    )
+)
+def semantic_tokens(ls: PythonLanguageServer, params: SemanticTokensParams):
+    """See https://microsoft.github.io/language-server-protocol/specification#textDocument_semanticTokens
+    for details on how semantic tokens are encoded."""
+    
+    TOKENS = re.compile('".*"(?=:)')
+    
+    uri = params.text_document.uri
+    doc = ls.workspace.get_document(uri)
+
+    last_line = 0
+    last_start = 0
+
+    data = []
+
+    for lineno, line in enumerate(doc.lines):
+        last_start = 0
+
+        for match in TOKENS.finditer(line):
+            start, end = match.span()
+            data += [
+                (lineno - last_line),
+                (start - last_start),
+                (end - start),
+                0, 
+                0
+            ]
+
+            last_line = lineno
+            last_start = start
+
+    return SemanticTokens(data=data)
+
+
+
+@python_server.command(PythonLanguageServer.CMD_PROGRESS)
+async def progress(ls: PythonLanguageServer, *args):
+    """Create and start the progress on the client."""
+    token = 'token'
+    # Create
+    await ls.progress.create_async(token)
+    # Begin
+    ls.progress.begin(token, WorkDoneProgressBegin(title='Indexing', percentage=0))
+    # Report
+    for i in range(1, 10):
+        ls.progress.report(
+            token,
+            WorkDoneProgressReport(message=f'{i * 10}%', percentage= i * 10),
+        )
+        await asyncio.sleep(2)
+    # End
+    ls.progress.end(token, WorkDoneProgressEnd(message='Finished'))
+
+
+@python_server.command(PythonLanguageServer.CMD_REGISTER_COMPLETIONS)
+async def register_completions(ls: PythonLanguageServer, *args):
+    """Register completions method on the client."""
+    params = RegistrationParams(registrations=[
+                Registration(
+                    id=str(uuid.uuid4()),
+                    method=COMPLETION,
+                    register_options={"triggerCharacters": "[':']"})
+             ])
+    response = await ls.register_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully registered completions method')
+    else:
+        ls.show_message('Error happened during completions registration.',
+                        MessageType.Error)
+
+
+@python_server.command(PythonLanguageServer.CMD_SHOW_CONFIGURATION_ASYNC)
+async def show_configuration_async(ls: PythonLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using coroutines."""
+    try:
+        config = await ls.get_configuration_async(
+            ConfigurationParams(items=[
+                ConfigurationItem(
+                    scope_uri='',
+                    section=PythonLanguageServer.CONFIGURATION_SECTION)
+        ]))
+
+        example_config = config[0].get('exampleConfiguration')
+
+        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+    except Exception as e:
+        ls.show_message_log(f'Error ocurred: {e}')
+
+
+@python_server.command(PythonLanguageServer.CMD_SHOW_CONFIGURATION_CALLBACK)
+def show_configuration_callback(ls: PythonLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using callback."""
+    def _config_callback(config):
+        try:
+            example_config = config[0].get('exampleConfiguration')
+
+            ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+        except Exception as e:
+            ls.show_message_log(f'Error ocurred: {e}')
+
+    ls.get_configuration(ConfigurationParams(items=[
+        ConfigurationItem(
+            scope_uri='',
+            section=PythonLanguageServer.CONFIGURATION_SECTION)
+    ]), _config_callback)
+
+
+@python_server.thread()
+@python_server.command(PythonLanguageServer.CMD_SHOW_CONFIGURATION_THREAD)
+def show_configuration_thread(ls: PythonLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using thread pool."""
+    try:
+        config = ls.get_configuration(ConfigurationParams(items=[
+            ConfigurationItem(
+                scope_uri='',
+                section=PythonLanguageServer.CONFIGURATION_SECTION)
+        ])).result(2)
+
+        example_config = config[0].get('exampleConfiguration')
+
+        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+    except Exception as e:
+        ls.show_message_log(f'Error ocurred: {e}')
+
+
+@python_server.command(PythonLanguageServer.CMD_UNREGISTER_COMPLETIONS)
+async def unregister_completions(ls: PythonLanguageServer, *args):
+    """Unregister completions method on the client."""
+    params = UnregistrationParams(unregisterations=[
+        Unregistration(id=str(uuid.uuid4()), method=COMPLETION)
+    ])
+    response = await ls.unregister_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully unregistered completions method')
+    else:
+        ls.show_message('Error happened during completions unregistration.',
+                        MessageType.Error)
